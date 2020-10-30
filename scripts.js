8f99e543d4c269e7bddafd4292b9fb68746e7fac
Antonio Cham <antoniowlcham2@gmail.com>
2020-10-30 12:09:06 +0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+} 
+
+docReady(function(){
+var result = document.getElementById('result');
+var lastResult, countResults = 0;
+
+var html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader", { fps: 10, qrbox: 250 });
+
+function onScanSuccess(qrCodeMessage){
+    if (qrCodeMessage !== lastResult) {
+        ++countResults;
+        lastResult = qrCodeMessage;
+        resultContainer.innerHTML += `<div>[${countResults}] - ${qrCodeMessage}</div>`;
+        
+        // Optional: To close the QR code scannign after the result is found
+        html5QrcodeScanner.clear();
+    }
+}
+
+function onScanError(errorMessage){
+    console.log(`QR error  = ${error}`);
+}
+
+
+html5QrcodeScanner.render(onScanSuccess, onScanError);
+});
